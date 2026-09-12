a07498309985e92ea8d490f234952c04bc8738d9
novix-vertex <167081171+novix-vertex@users.noreply.github.com>
2026-09-12 13:39:47 +0530

    get my post details api

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -27,7 +27,7 @@ const createPostController = async (req, res) => {
             user: decoded.id
         });
 
-        res.status(201).json({
+        return res.status(201).json({
             message: "post created successfully",
             post
         });
@@ -51,14 +51,14 @@ const getPostsController = async (req, res) => {
     }
 
     try {
-        console.log("token",token);
+        console.log("token", token);
         const decode = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("decode",decode);
-            
-            const posts = await postModel.find({ user: decode.id });
-            console.log("post",posts);
+        console.log("decode", decode);
 
-        res.status(200).json({
+        const posts = await postModel.find({ user: decode.id });
+        console.log("post", posts);
+
+        return res.status(200).json({
             message: "Posts fetched successfully.",
             posts
         })
@@ -69,9 +69,52 @@ const getPostsController = async (req, res) => {
     }
 }
 
+const getPostDetailController = async (req, res) => {
+
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not found! Unauthorized Access"
+        })
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decode.id;
+        const postId = req.params.postId;
+        
+        const post = await postModel.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found"
+            })
+        }
+
+        const isAuthorized = post.user.toString() === userId;
+
+        if (!isAuthorized) {
+            return res.status(403).json({
+                message: "You're not authorized to access this post content."
+            })
+        }
+
+        return res.status(200).json({
+            message: "Post details fetched successfully.",
+            post
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Token mismatched! Unauthorized Access"
+        })
+    }
+}
+
 
 
 module.exports = {
     createPostController,
-    getPostsController
+    getPostsController,
+    getPostDetailController
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createPostController, getPostsController } = require("../controllers/post.controller");
+const { createPostController, getPostsController, getPostDetailController } = require("../controllers/post.controller");
 const upload = require("../config/multer.config");
 
 const postRouter = express.Router();
 
 postRouter.post("/", upload.single("image"), createPostController);
-postRouter.get("/",getPostsController);
+postRouter.get("/", getPostsController);
+postRouter.get("/details/:postId", getPostDetailController);
 
 module.exports = postRouter;
